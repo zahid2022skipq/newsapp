f8347ae97d86574531b0edfcdc1873a4f7929801
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-14 14:38:27 +0500

    Added Navbar with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
 import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
   }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 
 export default class Navbar extends Component {
-  static propTypes = { second: third };
+  static propTypes = {};
 
   render() {
     return (
