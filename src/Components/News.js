d2b4ce04b95ca2b3ebbe227dda21a5ea9a319531
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-14 16:33:33 +0500

    Hardcoding static components

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
-    return <div>News Component</div>;
+    return (
+      <div className="container my-4">
+        <h2>Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-3 mx-2">
+            <NewsItem />
+          </div>
+          <div className="col-md-3 mx-2">
+            <NewsItem />
+          </div>
+          <div className="col-md-3 mx-2">
+            <NewsItem />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
