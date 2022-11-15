f0a43abc74ca1d0064f3af3f572169d7348725b1
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-15 12:10:33 +0500

    Added Fetch Url to fetch data dynamically

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -8,8 +8,12 @@ export default class NewsItem extends Component {
         <div className="card" style={{ width: "18rem" }}>
           <img src={`${imageURL}`} className="card-img-top" alt={title} />
           <div className="card-body">
-            <h5 className="card-title">{title.substring(0, 50) + "..."}</h5>
-            <p className="card-text">{description.substring(0, 100) + "..."}</p>
+            <h5 className="card-title">
+              {title ? title.substring(0, 50) + "..." : ""}
+            </h5>
+            <p className="card-text">
+              {description ? description.substring(0, 100) + "..." : ""}
+            </p>
             <a href="/" className="btn btn-primary">
               Read More
             </a>
