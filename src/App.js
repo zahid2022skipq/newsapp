5cedcc4611689b9679652175307a6abe0483c141
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-16 13:17:28 +0500

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ export default class App extends Component {
     return (
       <div className="container text-center">
         <Navbar />
-        <div className="my-3 select">
-          <h4>Select News Type</h4>
-          <this.NewsSelect />
-          <p>{this.state.newsType}</p>
-        </div>
         <News />
       </div>
     );
