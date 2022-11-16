f292d2ebb7fb4922e1a3c4dc4cc15d3d560e68b8
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-16 11:41:41 +0500

    Installed react-select package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import reactSelect from "react-select";
 
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+import Select from "react-select/dist/declarations/src/Select";
 
 export default class App extends Component {
   constructor() {
@@ -11,6 +13,18 @@ export default class App extends Component {
       country: "us",
     };
   }
+
+  options = [
+    { value: "us", label: "United States" },
+    { value: "pakistan", label: "Pakistan" },
+    { value: "india", label: "India" },
+    { value: "china", label: "China" },
+    { value: "australia", label: "Australia" },
+  ];
+
+  NewsSelect = () => {
+    return <Select options={this.options} />;
+  };
 
   componentDidMount() {
     this.setState({
