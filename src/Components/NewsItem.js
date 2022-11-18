b8c38fca5a0621e52e125044fe6dc47613dd0e25
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-18 12:32:18 +0500

    added default image for no image

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,15 @@ export default class NewsItem extends Component {
     return (
       <div className="my-2">
         <div className="card">
-          <img src={`${imageURL}`} className="card-img-top" alt={title} />
+          <img
+            src={
+              imageURL
+                ? `${imageURL}`
+                : "https://scx2.b-cdn.net/gfx/news/hires/2022/powerful-linear-accele.jpg"
+            }
+            className="card-img-top"
+            alt={title}
+          />
           <div className="card-body">
             <h5 className="card-title">
               {title ? title.substring(0, 50) + "..." : ""}
